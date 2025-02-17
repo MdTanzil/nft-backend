d76b3309c0d47662acff25f059f7ec0333f9485a
MdTanzil <mdtanzil.py@gmail.com>
2025-02-18 01:57:24 +0600

    add frontend url

--- config/cors.js
+++ config/cors.js
@@ -2,6 +2,7 @@ const allowedOrigins = [
   "http://localhost:5174",
   "http://localhost:5000",
   "http://localhost:3000",
+  "https://nft-frontend-pi.vercel.app",
 
   //   "https://stocksculpt.web.app",
 ];
